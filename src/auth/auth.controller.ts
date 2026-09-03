f3521c236e3bbc2e6e665bcc0c9517b6169c459d
Aman-Das-git <Amandas.satlok@gmail.com>
2026-09-03 21:47:28 +0530

    ïntegrate login and registration api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,13 +3,17 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    // step 3 DI
+  // step 3 DI
   constructor(private readonly authService: AuthService) {}
 
-  
   // step 2
   @Post('create')
   async create(@Body() requestData: any) {
     return this.authService.create(requestData);
+  }
+
+  @Post('login')
+  async login(@Body() requestData: any) {
+    return this.authService.login(requestData);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { AuthModel } from './auth.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { isValidObjectId, Model } from 'mongoose';
+import { waitForDebugger } from 'inspector';
 @Injectable()
 export class AuthService {
   constructor(
     // step 4 DI of schema model
     @InjectModel('Auth') private readonly authModel: Model<AuthModel>,
-  ) {}
+  ) { }
 
   //step 5
   async create(requestData: any): Promise<AuthModel> {
@@ -20,4 +21,16 @@ export class AuthService {
     //step 7 save a new row in db
     return await createdAuth.save();
   }
+
+  async login (requestData: any){
+    const user = await this.authModel.findOne({
+      mobile: requestData.mobile,
+      password: requestData.password,
+    });
+
+    return user;
+  }
+
+
+
 }
